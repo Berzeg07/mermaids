31e3b095d55028ef498661958d5359cd0d63c2df
Aslan <berezg86@gmail.com>
2020-09-07 14:44:35 +0300

    таблица в корзине

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,15 +2,18 @@ module.exports = function () {
 	$.gulp.task('sass', function () {
 		return $.gulp.src('src/static/css/main.scss')
 			.pipe($.gp.sourcemaps.init())
-			.pipe($.gp.autoprefixer({
-				browsers: ['last 10 versions']
-			}))
+			// .pipe($.gp.autoprefixer({
+			// 	browsers: ['last 10 versions']
+			// }))
 			.on("error", $.gp.notify.onError({
 				message: "Error: <%= error.message %>",
 				title: "style"
 			}))
 			// .pipe($.gp.csso())
 			.pipe($.gp.sass({ outputStyle: 'expanded' }))
+			.pipe($.gp.autoprefixer({
+				browsers: ['last 10 versions']
+			}))
 			// .pipe($.gp.sourcemaps.write('./'))
 			.pipe($.gulp.dest('build/css/'))
 			// Минифицированная версия
@@ -18,9 +21,10 @@ module.exports = function () {
 			.pipe($.gp.rename('main.min.css'))
 			.pipe($.gp.sourcemaps.write('./'))
 			.pipe($.gulp.dest('build/css/'))
+			.on('end', $.bs.reload);
 
-			.pipe($.bs.reload({
-				stream: true
-			}));
+		// .pipe($.bs.reload({
+		// 	stream: true
+		// }));
 	});
 }
